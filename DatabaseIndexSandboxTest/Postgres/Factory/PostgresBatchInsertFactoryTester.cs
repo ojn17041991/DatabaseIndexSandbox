5835f6686ccaec6a63a2e6ec1d739dd8e91be014
Oliver <olivernicholls91@hotmail.com>
2022-12-30 10:00:43 +0000

    Small changes to folder structure.

--- DatabaseIndexSandboxTest/Postgres/Factory/PostgresBatchInsertFactoryTester.cs
+++ DatabaseIndexSandboxTest/Postgres/Factory/PostgresBatchInsertFactoryTester.cs
@@ -1,4 +1,4 @@
-﻿using DatabaseIndexSandbox.Abstract.DB.Factories;
+﻿using DatabaseIndexSandbox.Abstract.DB.Factories.NonQueries.Inserts;
 using DatabaseIndexSandbox.Abstract.DB.Queries;
 using DatabaseIndexSandbox.Postgres.Factories;
 using DatabaseIndexSandboxTest.Config;
